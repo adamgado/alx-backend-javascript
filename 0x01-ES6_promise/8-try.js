f535b8004d893ffa8dd41161411e86fe1d5d1d41
adamgado <adamgado33@gmail.com>
2024-10-01 20:16:33 +0000

    8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
+}
+
+export default divideFunction;
