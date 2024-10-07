85544962af5262f344ee05856913eaa6d7e8cd87
adamgado <adamgado33@gmail.com>
2024-10-07 18:42:37 +0000

    0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
 function getListStudents() {
-  const obj = [
+  const student_list = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
-  return obj;
+  return student_list;
 }
 
 export default getListStudents;
