01696325b1bacd37bc1032a03673799bae6284ff
adamgado <adamgado33@gmail.com>
2024-10-07 18:38:30 +0000

    1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(student_list) {
+  let id_list = [];
+  if (student_list instanceof Array) {
+    id_list = student_list.map((item) => item.id);
+  }
+  return id_list;
+}
