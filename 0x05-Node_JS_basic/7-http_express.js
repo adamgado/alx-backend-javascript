734673e4de07f760851f084e5a6ce6671e2720d7
adamgado <adamgado33@gmail.com>
2024-11-25 18:01:12 +0000

    7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,18 @@
+const studentcount = require('./3-read_file_async');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', async (req, res) => {
+  const message = 'This is the list of our students\n';
+  try {
+    const studentslist = await studentcount(process.argv.slice(2)[0]);
+    res.send(`${message}${studentslist.join('\n')}`);
+  } catch (error) {
+    res.send(`${message}${error.message}`);
+  }
+});
+app.listen(1245, () => {});
+module.exports = app;
