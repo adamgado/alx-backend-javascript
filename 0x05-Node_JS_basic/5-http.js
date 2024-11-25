86a943c5c4a761cf2e8823ebabc0355ad0755c1a
adamgado <adamgado33@gmail.com>
2024-11-25 17:52:42 +0000

    5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,8 +1,7 @@
 const http = require('http');
 
 const countStudents = require('./3-read_file_async');
-const hostname = '127.0.0.1';
-const port = 1245;
+
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -14,14 +13,12 @@ const app = http.createServer(async (req, res) => {
     try {
       const studentslist = await countStudents(process.argv.slice(2)[0]);
       res.end(`${studentslist.join('\n')}`);
-    } catch (err) {
-      res.end(err.message);
+    } catch (error) {
+      res.end(error.message);
     }
   }
   res.statusCode = 404;
   res.end();
 });
-
-app.listen(port, hostname, () => {});
-
+app.listen(1245, '127.0.0.1', () => {});
 module.exports = app;
