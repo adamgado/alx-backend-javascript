e8fbafc98c21d9a1fb74db549ce02455891b05b8
adamgado <adamgado33@gmail.com>
2024-11-25 17:46:41 +0000

    5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,34 +1,23 @@
 const http = require('http');
 
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
-
-const hostname = '127.0.0.1';
-const port = 1245;
-
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
   const { url } = req;
-
   if (url === '/') {
     res.write('Hello Holberton School!');
   } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
+      const studentslist = await countStudents(process.argv.slice(2)[0]);
+      res.end(`${studentslist.join('\n')}`);
+    } catch (err) {
+      res.end(err.message);
     }
   }
   res.statusCode = 404;
   res.end();
 });
-
-app.listen(port, hostname, () => {});
-
+app.listen(1245, '127.0.0.1', () => {});
 module.exports = app;
