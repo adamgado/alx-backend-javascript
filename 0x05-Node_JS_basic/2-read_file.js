7ec659b9df6dbc526c29a186bd879869fd3dcde7
adamgado <adamgado33@gmail.com>
2024-11-25 16:29:30 +0000

    2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+function countStudents(path) {
+  let content;
+  try {
+    content = fs.readFileSync(path);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  content = content.toString().split('\n');
+  let studentslist = content.filter((item) => item);
+  studentslist = studentslist.map((item) => item.split(','));
+  const studentsnumber = studentslist.length ? studentslist.length - 1 : 0;
+  console.log(`Number of students: ${studentsnumber}`);
+  const fields = {};
+  for (const a in studentslist) {
+    if (a !== 0) {
+      if (!fields[studentslist[a][3]]) fields[studentslist[a][3]] = [];
+
+      fields[studentslist[a][3]].push(studentslist[a][0]);
+    }
+  }
+  delete fields.field;
+  for (const k of Object.keys(fields)) {
+    console.log(
+      `Number of students in ${k}: ${fields[k].length}. List: ${fields[k].join(', ')}`,
+    );
+  }
+}
+module.exports = countStudents;
